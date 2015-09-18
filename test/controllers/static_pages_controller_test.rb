aabb4f34fc99818ede080232490a9025bc6b7826
toquamer <toquamer@gmail.com>
2015-09-18 19:16:27 +0200

    Update tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Welcome to guesthouse_name"
+    assert_select "title", "Welcome to guesthouse_name!"
   end
 
   test "should get about" do
